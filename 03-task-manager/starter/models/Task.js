14540665ebc8729d10202ee808f7abb9db06ce71
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-10 21:39:38 +0300

    Create task.

---
+++ 03-task-manager/starter/models/Task.js
@@ -0,0 +1,10 @@
+// Set Task database model
+
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    name: String,
+    completed: Boolean
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
