f740202b5271160e582618a5c6297a922a4e8bca
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-10 19:52:06 +0300

    Initial app structure.

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require('express');
+const taskRoutes = require('./routes/task-routes');
+
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// routes
+app.use('/api/v1/tasks', taskRoutes);
+
+app.listen(port, () => {
+    console.log("Server is started on port 3000");
+});
