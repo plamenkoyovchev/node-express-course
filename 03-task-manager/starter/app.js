cb10dd2efff5869a251d2d5b40fb73798ee1114f
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-10 20:31:50 +0300

    Use the absolute path of the dir.

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const taskRoutes = require('./routes/task-routes');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 // middleware
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // routes
