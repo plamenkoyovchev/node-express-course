b81c34cc49d29fefc78547d6696f2190dccbe0b0
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-10 21:52:15 +0300

    Add basic validation.

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -3,8 +3,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+        maxlength: [25, 'Name max length should be 25 chars'],
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
