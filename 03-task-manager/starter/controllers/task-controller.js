cb8f60afe74c43ae0818ea0c0e85959542e2442b
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-11 08:54:08 +0300

    Configure async wrapper.

--- 03-task-manager/starter/controllers/task-controller.js
+++ 03-task-manager/starter/controllers/task-controller.js
@@ -1,39 +1,33 @@
 const Task = require('../models/Task');
+const asyncWrapper = require('../middleware/async');
 
-const getAllTasks = async (req, res) => {
-    try {
+const getAllTasks = asyncWrapper(
+    async (req, res) => {
         const tasks = await Task.find({});
         res.status(200).json({ tasks });
-    } catch (error) {
-        res.status(500).json({ msg: 'Error loading tasks' });
     }
-};
+);
 
-const createTask = async (req, res) => {
-    try {
+const createTask = asyncWrapper(
+    async (req, res) => {
         const task = await Task.create(req.body);
         res.status(201).json({ task });
-    } catch (error) {
-        // TODO: extract validation messages only
-        res.status(500).json({ msg: error });
     }
-};
+);
 
-const getTask = async (req, res) => {
-    try {
+const getTask = asyncWrapper(
+    async (req, res) => {
         const task = await Task.findById(req.params.id).exec();
         if (!task) {
             return res.status(404).json({ msg: 'Task not found' });
         }
 
         res.status(200).json({ task });
-    } catch (error) {
-        res.status(500).json({ msg: error });
     }
-};
+);
 
-const updateTask = async (req, res) => {
-    try {
+const updateTask = asyncWrapper(
+    async (req, res) => {
         const taskToUpdate = await Task.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
@@ -43,23 +37,19 @@ const updateTask = async (req, res) => {
         }
 
         res.status(200).send({ task: taskToUpdate });
-    } catch (error) {
-        res.status(500).json({ msg: error });
     }
-};
+);
 
-const deleteTask = async (req, res) => {
-    try {
+const deleteTask = asyncWrapper(
+    async (req, res) => {
         const taskToDelete = await Task.findOneAndDelete({ _id: req.params.id });
         if (!taskToDelete) {
             return res.status(404).json({ msg: "Task not found." });
         }
 
         res.status(200).send(true);
-    } catch (error) {
-        res.status(500).json({ msg: error });
     }
-};
+);
 
 module.exports = {
     getAllTasks,
