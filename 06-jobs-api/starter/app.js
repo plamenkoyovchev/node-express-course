f93a41eb98574e32f1a98210519ebdf18b5aabc7
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-15 21:16:18 +0300

    Connect to database.

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 require('express-async-errors');
+
 const express = require('express');
 const app = express();
+
+// Connect to db
+const connectToDb = require('./db/connect');
+
+// routes
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -11,9 +19,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +29,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectToDb(process.env.MONGO_CONNECTION_STRING);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
