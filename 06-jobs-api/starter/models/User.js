916f4dbcba09943e8c9d15a3cbd873dc9dc2a78f
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-16 14:01:19 +0300

    Hash user password before save.

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -18,12 +19,19 @@ const UserSchema = new mongoose.Schema({
         ],
         unique: true
     },
-    name: {
+    password: {
         type: String,
         required: [true, 'Password is required'],
-        minLength: 6,
-        maxlength: 12
+        minLength: 6
     }
+});
+
+// mongoose middleware
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
