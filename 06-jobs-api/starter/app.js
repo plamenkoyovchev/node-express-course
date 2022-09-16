d28f8ee247f31667ee196ae0dc15b0cf65fad6a1
plamenkoyovchev <plamenkooo@gmail.com>
2022-09-16 14:43:34 +0300

    Secure jobs route with authentication middleware.

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 // Connect to db
 const connectToDb = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
 
 // routes
 const authRouter = require('./routes/auth');
@@ -20,7 +21,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
